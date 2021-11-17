414a210b867b130d55a9f2bb9f4c38335639e911
Bobby Reay <rreay724@gmail.com>
2021-11-17 09:37:01 -0500

    added header and sidebar to video page

--- pages/videoPage.js
+++ pages/videoPage.js
@@ -1,12 +1,17 @@
 import YouTube from "react-youtube";
 import { useRouter } from "next/dist/client/router";
+import { Header, Sidebar } from "../components/index";
 
 function videoPage() {
   const router = useRouter();
   const { id } = router.query;
   return (
     <div>
-      <YouTube videoId={id} />
+      <Header />
+      <div className="flex">
+        <Sidebar />
+        <YouTube videoId={id} />
+      </div>
     </div>
   );
 }
