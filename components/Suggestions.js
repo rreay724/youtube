52741a720bcc80ef40e116d22f6ffda6cab4b60d
Bobby Reay <rreay724@gmail.com>
2021-11-29 13:19:11 -0500

    error messages

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,19 +1,24 @@
 import { SuggestionsThumbnail } from "../components/index";
 
 function Suggestions({ data }) {
+  console.log(data);
   return (
     <div className="w-suggestionWidth min-h-screen pr-10 hidden lg:inline-block">
-      {data
-        ? data.items.map((item) => (
-            <SuggestionsThumbnail
-              key={item.id.videoId}
-              id={item.id.videoId}
-              title={item.snippet?.title}
-              thumbnail={item.snippet?.thumbnails.medium.url}
-              channelTitle={item.snippet?.channelTitle}
-            />
-          ))
-        : null}
+      {!data.error ? (
+        data.items.map((item) => (
+          <SuggestionsThumbnail
+            key={item.id.videoId}
+            id={item.id.videoId}
+            title={item.snippet?.title}
+            thumbnail={item.snippet?.thumbnails.medium.url}
+            channelTitle={item.snippet?.channelTitle}
+          />
+        ))
+      ) : (
+        <p className="text-white pl-14 pt-10">
+          API Limit Reached. Cannot display suggestions
+        </p>
+      )}
     </div>
   );
 }
