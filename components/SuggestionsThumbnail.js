02af11dac275db91dd322b3f276d01ade1dee78d
Bobby Reay <rreay724@gmail.com>
2021-12-07 11:26:12 -0500

    can now click on suggested thumbnail and push to firestore

--- components/SuggestionsThumbnail.js
+++ components/SuggestionsThumbnail.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useRouter } from "next/dist/client/router";
 import { doc, setDoc, getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 function SuggestionsThumbnail({
   id,
@@ -16,28 +17,43 @@ function SuggestionsThumbnail({
   // title !== null && titleSnippet === title.substring(0, 50) + "...";
   const router = useRouter();
   let today = new Date();
+  async function handleClick() {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    router.push({
+      pathname: "/suggestedVideoPage",
+      query: {
+        id: id,
+      },
+    });
+
+    if (user) {
+      const data = await fetch(
+        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=statistics&part=player&id=${id}&key=${process.env.NEXT_PUBLIC_API_KEY}`
+      ).then((res) => res.json());
+      const db = getFirestore();
+      console.log(data);
+
+      setDoc(doc(db, user.uid, id), {
+        userId: user.uid,
+        videoId: data.items[0].id,
+        thumbnail: data.items[0].snippet.thumbnails.medium.url,
+        title: data.items[0].snippet.title,
+        channelTitle: data.items[0].snippet.channelTitle,
+        viewCount: data.items[0].statistics.viewCount,
+        publishedAt: data.items[0].snippet.publishedAt,
+        date: today,
+        thumbnailWidth: data.items[0].snippet.thumbnails.medium.width,
+        thumbnailHeight: data.items[0].snippet.thumbnails.medium.height,
+        description: data.items[0].snippet.description,
+      });
+    }
+  }
 
   return (
     <div
       className="flex pl-5 cursor-pointer items-top pb-0.5"
-      onClick={() => {
-        router.push({
-          pathname: "/suggestedVideoPage",
-          query: {
-            id: id,
-          },
-        });
-        // setDoc(doc(db, user.uid, id), {
-        //   userId: user.uid,
-        //   videoId: id,
-        //   thumbnail: thumbnail,
-        //   title: title,
-        //   channelTitle: channelTitle,
-        //   viewCount: viewCount,
-        //   publishedAt: publishedAt,
-        //   date: today,
-        // });
-      }}
+      onClick={handleClick}
     >
       <div className="flex-none">
         {thumbnail ? <Image src={thumbnail} width={190} height={116} /> : null}
